39c7b6a92de6632072e560b0e2f2e707c3143b4c
OmkarTerbhai <omkarterbhai@gmail.com>
2023-06-03 16:43:18 +0530

    Added rate limiter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const express = require('express');
-
-const { ServerConfig } = require('./config');
+const rateLimit = require('express-rate-limit');
+const { ServerConfig, Logger } = require('./config');
 const apiRoutes = require('./routes');
 
 const {Auth} = require('./utils/common')
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000,
+    max: 3
+});
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
+app.use(limiter);
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
-    console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    console.log(Auth.verifyToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NywiZW1haWwiOiJhYmNmQHd5ci5vcGoiLCJpYXQiOjE2ODU3MDI1OTEsImV4cCI6MTY4NTcwNjE5MX0.MACjW0ZyFHqIzuQRqpSLdRWXbqtxCDhhb4IKw7bt3IU"));
+    Logger.info(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
 });
